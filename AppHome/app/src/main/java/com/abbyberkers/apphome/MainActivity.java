52ba8125f6f5eced5527508f5469fd15d98df520
Thomas Schouten <hollandpirates@gmail.com>
2016-03-05 17:08:16 +0100

    added buttons for last train

--- AppHome/app/src/main/java/com/abbyberkers/apphome/MainActivity.java
+++ AppHome/app/src/main/java/com/abbyberkers/apphome/MainActivity.java
@@ -29,55 +29,86 @@ public class MainActivity extends AppCompatActivity {
      * Buttons.
      */
 
-    public void sendTextRoosendaal(View view){
-        Calendar c = Calendar.getInstance();
-        c.set(Calendar.SECOND,0);
-        c.set(Calendar.MILLISECOND,0);
-
-        //time next train
-        int minutes = c.get(Calendar.MINUTE);
-        if (minutes<31) {
-            minutes=31; //next train at :31
-        } else {
-            minutes = 61; //add up to the next train departure at :01
-        }
-        c.set(Calendar.MINUTE,minutes);
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm",java.util.Locale.getDefault());
-        //String nextTrain = simpleDateFormat.format(c.getTime());
-        c.add(Calendar.MINUTE,89); //time I need to get home
-        String ETA = "ETA "+simpleDateFormat.format(c.getTime());
-
-        Intent sendIntent = new Intent();
-        sendIntent.setAction(Intent.ACTION_SEND);
-        sendIntent.putExtra(Intent.EXTRA_TEXT, ETA);
-        sendIntent.setType("text/plain");
-        sendIntent.setPackage("com.whatsapp");
-        startActivity(sendIntent);
+    public void nextTrainRoosendaal(View view) {
+        sendText("Roosendaal", true);
     }
 
-    public void sendTextHeeze(View view){
+    public void lastTrainRoosendaal(View view) {
+        sendText("Roosendaal", false);
+    }
+
+    public void nextTrainHeeze(View view) {
+        sendText("Heeze", true);
+    }
+
+    public void lastTrainHeeze(View view) {
+        sendText("Heeze", false);
+    }
+
+    /**
+     * Sending of text
+     *
+     * @param dest      destination of you.
+     * @param nextTrain taking next or took last train
+     */
+
+    public void sendText(String dest, boolean nextTrain) {
+        //initialise calendar with current time, trimmed
         Calendar c = Calendar.getInstance();
-        c.set(Calendar.SECOND,0);
-        c.set(Calendar.MILLISECOND,0);
-
-        //time next train
+        c.set(Calendar.SECOND, 0);
+        c.set(Calendar.MILLISECOND, 0);
+        //add the minutes until the next train
         int minutes = c.get(Calendar.MINUTE);
-        if (minutes<34) {
-            minutes=34; //next train at :34
-        } else {
-            minutes = 64; //add up to the next train departure at :04
+
+        int trainTime = 0;
+        switch (dest) {
+            case "Roosendaal":
+                trainTime = 31; //train departs at :31
+                break;
+            case "Heeze":
+                trainTime = 34; //:34
+                break;
+            default:
+                Toast.makeText(this, "I don't know the destination!", Toast.LENGTH_SHORT).show();
+                break;
         }
-        c.set(Calendar.MINUTE,minutes);
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm",java.util.Locale.getDefault());
-        //String nextTrain = simpleDateFormat.format(c.getTime());
-//        c.add(Calendar.MINUTE,89); //time I need to get home
-//        String ETA = "ETA "+simpleDateFormat.format(c.getTime());
 
-        String train = "Trein van " + simpleDateFormat.format(c.getTime());
+        if (nextTrain) { //taking next train
+            if (minutes < trainTime) {
+                minutes = trainTime; //next train at e.g. :31
+            } else {
+                minutes = 30 + trainTime; //add up to the next train departure at e.g. :01
+            }
+        } else { //took last train
+            if (minutes < trainTime) {
+                minutes = trainTime - 30;
+            } else {
+                minutes = trainTime;
+            }
+        }
 
+        c.set(Calendar.MINUTE, minutes); //set time of next train
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(
+                "HH:mm", java.util.Locale.getDefault());
+
+        String message = "hi.";
+        switch (dest) {
+            case "Roosendaal":
+                c.add(Calendar.MINUTE, 89); //add time I need to get home
+                message = "ETA " + simpleDateFormat.format(c.getTime());
+                break;
+            case "Heeze":
+                message = "Trein van " + simpleDateFormat.format(c.getTime());
+                break;
+            default:
+                Toast.makeText(this, "I don't know the destination!", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        //send to whatsapp
         Intent sendIntent = new Intent();
         sendIntent.setAction(Intent.ACTION_SEND);
-        sendIntent.putExtra(Intent.EXTRA_TEXT, train);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, message);
         sendIntent.setType("text/plain");
         sendIntent.setPackage("com.whatsapp");
         startActivity(sendIntent);
