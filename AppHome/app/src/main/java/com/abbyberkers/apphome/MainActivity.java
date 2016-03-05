46931ac079355c0699e83c5eeb9fad18f2e41c0c
Thomas Schouten <hollandpirates@gmail.com>
2016-03-05 19:39:51 +0100

    Added last train checkbox and fixed time bug

--- AppHome/app/src/main/java/com/abbyberkers/apphome/MainActivity.java
+++ AppHome/app/src/main/java/com/abbyberkers/apphome/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CheckBox;
 import android.widget.NumberPicker;
 import android.widget.Toast;
 
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     NumberPicker npTo;
     int from = 0; //default from Eindhoven
     int to;
+    boolean boxChecked; //check if checkbox checked
 
     String[] cities = new String[]{"Eindhoven", "Heeze", "Roosendaal"};
 
@@ -63,31 +65,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * buttons ------------------------------------------------------
+     * Sends the text, called on buttonclick
+     *
+     * @param view button Send
      */
 
-    public void sendText(View view){
+    public void sendText(View view) {
 
         String message = "Yay! I am default.";
 
-        if(from == 0){
-            if(to == 1){
-                message = "Trein van " + time(34, 0);
-            } else if(to == 2){
-                message = "ETA " + time(31, 89);
+        int EHV = 0;
+        int Heeze = 1;
+        int RDaal = 2;
+
+        if (from == EHV) {
+            if (to == Heeze) {
+                message = "Trein van " + time(4, 0);
+            } else if (to == RDaal) {
+                message = "ETA " + time(1, 89);
 
             }
-        } else if(from == 1){
-            if(to == 0){
-                message = "Eindhoven ETA " + time(45,15);
-            } else if(to == 2){
-                message = "Aiming for the " + time(31, 0) + " Eindhoven train.";
+        } else if (from == Heeze) {
+            if (to == EHV) {
+                message = "Eindhoven ETA " + time(15, 15);
+            } else if (to == RDaal) {
+                message = "Aiming for the " + time(1, 0) + " Eindhoven train.";
             }
-        } else if(from == 2) {
-            if(to == 0){
-                message = "ETA " + time(50, 100);
-            } else if(to == 1){
-                message = "ETA " + time(50, 113);
+        } else if (from == RDaal) {
+            if (to == EHV) {
+                message = "ETA " + time(20, 70);
+            } else if (to == Heeze) {
+                message = "ETA " + time(20, 113);
             }
         }
 
@@ -95,34 +103,70 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    String time(int depart, int travel){
+    /**
+     * departure time should be the first departure time in a given hour
+     *
+     * @param depart Train departure time
+     * @param travel Total travel time
+     * @return ETA destination, or departure time if travel=0
+     */
+
+    String time(int depart, int travel) {
+        //initialise calendar with current time and trim
         Calendar c = Calendar.getInstance();
         c.set(Calendar.SECOND, 0);
         c.set(Calendar.MILLISECOND, 0);
 
         //time next train
         int minutes = c.get(Calendar.MINUTE);
-        if (minutes < depart) {
-            minutes = depart; //next train at :31
-        } else {
-            minutes = depart + 30; //add up to the next train departure at :01
+        if (!boxChecked) { //taking next train
+            if (minutes < depart) {
+                minutes = depart; //next train at e.g. :20
+            } else if (minutes < depart + 30) {
+                minutes = 30 + depart; //add up to the next train departure at e.g. :50
+            } else { //minutes>depart+30
+                minutes = depart + 60; //train departure in the next hour, :20
+            }
+        } else { //took last train
+            if (minutes < depart) {
+                minutes = depart - 30; //e.g. the :50 of last hour
+            } else if (minutes < depart + 30) {
+                minutes = depart; //took the :20 train
+            } else { //minutes >depart+30
+                minutes = depart + 30;
+            }
         }
         c.set(Calendar.MINUTE, minutes);
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm", java.util.Locale.getDefault());
-        if(travel > 0) {
+        if (travel > 0) { //if we want to know arrival time, otherwise return departure time
             c.add(Calendar.MINUTE, travel); //time I need to get home
         }
 
         return simpleDateFormat.format(c.getTime());
     }
 
-    public void sendText(String text){
+    /**
+     * send message to whatsapp
+     *
+     * @param text the message
+     */
+    public void sendText(String text) {
         Intent sendIntent = new Intent();
         sendIntent.setAction(Intent.ACTION_SEND);
         sendIntent.putExtra(Intent.EXTRA_TEXT, text);
         sendIntent.setType("text/plain");
         sendIntent.setPackage("com.whatsapp");
         startActivity(sendIntent);
+    }
+
+    /**
+     * if checkbox hit
+     *
+     * @param v view of checkbox
+     */
+    public void checkedLastTrain(View v) {
+        CheckBox checkbox = (CheckBox) findViewById(R.id.lastCheck);
+        boxChecked = checkbox.isChecked();
     }
 
 }
