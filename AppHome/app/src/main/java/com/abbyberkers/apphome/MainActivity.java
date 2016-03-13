c576d72d71b4d5ee1f7a7c993c2a96918412c72f
Thomas Schouten <hollandpirates@gmail.com>
2016-03-13 21:04:41 +0100

    returns 403, not logged in

--- AppHome/app/src/main/java/com/abbyberkers/apphome/MainActivity.java
+++ AppHome/app/src/main/java/com/abbyberkers/apphome/MainActivity.java
@@ -4,15 +4,23 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.NumberPicker;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
@@ -22,12 +30,23 @@ public class MainActivity extends AppCompatActivity {
     int to;
     int depart; //departure numberpicker value
 
+    TextView responseView;
+    ProgressBar progressBar;
+    static final String API_KEY = "sO-65AZxuErJmmC28eIRB85aos7oGVJ0C6tOZI9YeHDPLXeEv1nfBg";
+    static final String API_URL = "http://webservices.ns.nl/ns-api-treinplanner?";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //SETUP NS STUFF
+        responseView = (TextView) findViewById(R.id.responseView);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        //------
+
 
         String[] cities = new String[]{"Eindhoven", "Heeze", "Roosendaal"};
 
@@ -84,7 +103,75 @@ public class MainActivity extends AppCompatActivity {
                 depart = newVal;
             }
         });
+
+        new RetrieveFeedTask().execute();
     }
+
+    class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
+
+        private Exception exception;
+
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+            responseView.setText("");
+        }
+
+        protected String doInBackground(Void... urls) {
+            String fromStation = "Roosendaal";
+            String toStation = "Eindhoven";
+
+            try {
+//                URL url = new URL(API_URL + "fromStation=" + fromStation +
+//                        "&toStation=" + toStation);//+ "&apiKey=" + API_KEY);
+                URL url = new URL("http://webservices.ns.nl/ns-api-treinplanner?fromStation=Utrecht+Centraal&toStation=Wierden&departure=true");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                } finally {
+                    //urlConnection.getErrorStream();
+                    urlConnection.disconnect();
+                }
+            } catch (Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String response) {
+            if (response == null) {
+                response = "THERE WAS AN ERROR";
+            }
+            progressBar.setVisibility(View.GONE);
+            Log.i("INFO", response);
+            responseView.setText(response);
+            // TODO: check this.exception
+            // TODO: do something with the feed
+
+//            try {
+//                JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
+//                String requestID = object.getString("requestId");
+//                int likelihood = object.getInt("likelihood");
+//                JSONArray photos = object.getJSONArray("photos");
+//                .
+//                .
+//                .
+//                .
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+        }
+    }
+
+
+    //*****************normal stuff****************
 
     /**
      * Update the time picker when selecting a new destination
