0ce58bd4e486a6f945e006f5950396520e021802
Thomas Schouten <hollandpirates@gmail.com>
2016-03-19 22:28:49 +0100

    fixed #10 live departure times for all travels

--- AppHome/app/src/main/java/com/abbyberkers/apphome/MainActivity.java
+++ AppHome/app/src/main/java/com/abbyberkers/apphome/MainActivity.java
@@ -12,7 +12,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.NumberPicker;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
@@ -45,14 +44,21 @@ import javax.xml.xpath.XPathFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    int from = 0; //default from Eindhoven
+    int from; //default from Eindhoven
     int to;
     int depart; //departure numberpicker value
 
-    TextView responseView;
     ProgressBar progressBar;
-    String response; //set after getting xml from ns, used by getNSDepTimes TODO link
+    String response; //set after getting xml from ns, used by
+    /**
+     * response is used by {@link #getNSDepartures()} and set by the AsyncTask
+     */
 
+    /**
+     * OnCreate
+     *
+     * @param savedInstanceState bundle
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,11 +66,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //SETUP NS STUFF
-        responseView = (TextView) findViewById(R.id.responseView);
+        //progressbar when getting response from slow NS
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        //------
-
 
         String[] cities = new String[]{"Eindhoven", "Heeze", "Roosendaal"};
 
@@ -81,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
                 from = newVal;
-                updateDepartures();
+                new RetrieveFeedTask().execute(); //get xml from ns
             }
         });
 
@@ -99,7 +102,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
                 to = newVal;
-                updateDepartures();
+                new RetrieveFeedTask().execute(); //get xml from ns
             }
         });
 
@@ -121,82 +124,54 @@ public class MainActivity extends AppCompatActivity {
                 depart = newVal;
             }
         });
-
-        new RetrieveFeedTask().execute();
     }
 
-    class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
-
-        protected void onPreExecute() {
-            progressBar.setVisibility(View.VISIBLE);
-            responseView.setText("");
-        }
-
-        protected String doInBackground(Void... urls) {
-            InputStream in;
-            int resCode;
-
-            try {
-                //http://t.m.schouten@student.tue.nl:sO-65AZxuErJmmC28eIRB85aos7oGVJ0C6tOZI9YeHDPLXeEv1nfBg@webservices.ns.nl/ns-api-treinplanner?fromStation=Roosendaal&toStation=Eindhoven
-                URL url = new URL("http://webservices.ns.nl/ns-api-treinplanner?fromStation=Roosendaal&toStation=Eindhoven");
-
-//                String userCredentials = "t.m.schouten@student.tue.nl:sO-65AZxuErJmmC28eIRB85aos7oGVJ0C6tOZI9YeHDPLXeEv1nfBg";
-//                String encoding = new String(android.util.Base64.encode(userCredentials.getBytes(), Base64.DEFAULT));
-//                encoding = encoding.replaceAll("\\s+",""); //because the base64 encoding doesn't work.
-
-                //encoded userCredentials with online encoder
-                String encoding = "dC5tLnNjaG91dGVuQHN0dWRlbnQudHVlLm5sOnNPLTY1QVp4dUVySm1tQzI4ZUlSQjg1YW9zN29HVkowQzZ0T1pJOVllSERQTFhlRXYxbmZCZw==";
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestProperty("Authorization", "Basic " + encoding);
-
-                try {
-                    resCode = urlConnection.getResponseCode();
-                    if (resCode == HttpURLConnection.HTTP_OK) {
-                        in = urlConnection.getInputStream();
-                    } else {
-                        Log.e("rescode", "rescode not ok");
-                        in = urlConnection.getErrorStream();
-                    }
-                    BufferedReader bufferedReader = new BufferedReader(
-                            new InputStreamReader(in));
-                    StringBuilder stringBuilder = new StringBuilder();
-                    String line;
-                    while ((line = bufferedReader.readLine()) != null) {
-                        stringBuilder.append(line).append("\n");
-                    }
-                    bufferedReader.close();
-                    return stringBuilder.toString();
-                }
-                finally{
-                    urlConnection.disconnect();
-                }
-            }
-            catch(Exception e) {
-                Log.e("ERROR", e.getMessage(), e);
-                return null;
-            }
-        }
-
-        protected void onPostExecute(String response) {
-//            if(response == null) {
-//                response = "THERE WAS AN ERROR";
-//            }
-            progressBar.setVisibility(View.GONE);
-            setResponse(response);
-        }
-    }
-
+    /**
+     * Set instance response from the ASyncTask
+     *
+     * @param response response passed
+     */
     public void setResponse(String response) {
         this.response = response;
-        //debug
-        String[] res = getNSDepartures();
     }
 
-    public String[] getNSDepartures() {
+    /**
+     * Convert the instance variables to and from, set by the numberpickers,
+     * to a string to be used in {@link AsyncTask()}
+     *
+     * @param toFrom the to or from variable
+     * @return string of city
+     */
+    public String convertToFromToString(int toFrom) {
+        String result;
+        switch (toFrom) {
+            case 0:
+                result = "Eindhoven";
+                break;
+            case 1:
+                result = "Heeze";
+                break;
+            case 2:
+                result = "Roosendaal";
+                break;
+            default:
+                result = "I don't know city";
+        }
+        return result;
+    }
+
+    /**
+     * Communicates with ASyncTask using the instance variables to, from and response
+     *
+     * @return Calendar[] with five current departures
+     */
+    public Calendar[] getNSDepartures() {
         if (response == null) {
+            Log.e("getNSdeps", "no response from ns");
             response = "No response from NS";
         }
         try {
+
             //create java DOM xml parser
             DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder;
@@ -224,6 +199,9 @@ public class MainActivity extends AppCompatActivity {
                 nsTimes.add(i, nodeList.item(i).getFirstChild().getNodeValue());
             }
 
+            Log.e("nstimes", Integer.toString(nsTimes.size()));
+            Log.e("forlnstimes", nsTimes.get(3));
+
             //get next departure time
             Calendar nextDepCal = departureTimeCal(20, 0); //uses instance variables to and from
             Date nextDepDate = nextDepCal.getTime();
@@ -239,7 +217,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
 
-            String[] depTimes = new String[5];
+            Calendar[] depTimes = new Calendar[5];
 
             if (nextIndex == -1) {
                 Log.e("nextIndex", "no next departure time!");
@@ -247,7 +225,7 @@ public class MainActivity extends AppCompatActivity {
                 //index is index of next dept time of all the xml deptimes in nsTimes
                 //get departure times around next time
                 for (int i = 0; i < depTimes.length; i++) {
-                    depTimes[i] = convertNSToString(nsTimes.get(nextIndex - 2 + i));
+                    depTimes[i] = convertNSToCal(nsTimes.get(nextIndex - 2 + i));
                 }
             }
 
@@ -259,19 +237,25 @@ public class MainActivity extends AppCompatActivity {
 //            }
 
             //test printing
-            String res = "";
-            for (String depTime : depTimes) {
-                res += "\n" + depTime;
-            }
-            responseView.setText(res);
+//            String res = "";
+//            for (String depTime : depTimes) {
+//                res += "\n" + convertCalendarToString(depTime);
+//            }
+//            responseView.setText(res);
 
             return depTimes;
         } catch (ParserConfigurationException | SAXException | IOException | XPathExpressionException e) {
             e.printStackTrace();
         }
-        return new String[]{" ", " ", " ", " ", " "};
+        return new Calendar[5]; //return default, null objects
     }
 
+    /**
+     * convert a time string in ns format to a date object
+     *
+     * @param nsTime time in ns format
+     * @return date object
+     */
     public Date convertNSToDate(String nsTime) {
         try {
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.getDefault());
@@ -282,20 +266,28 @@ public class MainActivity extends AppCompatActivity {
         return new Date(); //default
     }
 
-    public String convertNSToString(String nsTime) {
+    /**
+     * convert ns time string to calendar object, uses {@link #convertNSToDate(String)}
+     *
+     * @param nsTime time in ns format
+     * @return calendar object
+     */
+    public Calendar convertNSToCal(String nsTime) {
         Date date = convertNSToDate(nsTime);
         Calendar c = new GregorianCalendar();
         c.setTime(date);
-//        c.add(Calendar.HOUR, 1); //add one hour because +1:00 NS times
-        return convertCalendarToString(c);
+        return c;
     }
 
+    /**
+     * convert calendar object to string object in HH:mm format
+     * @param c calendar object
+     * @return string object
+     */
     public String convertCalendarToString(Calendar c) {
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm", java.util.Locale.getDefault());
         return sdf.format(c.getTime());
     }
-
-    //*****************normal stuff****************
 
     /**
      * Update the time picker when selecting a new destination
@@ -310,10 +302,18 @@ public class MainActivity extends AppCompatActivity {
         if (to == from) {
             setDividerColor(npDep, 0);
         } else {
-            setDividerColor(npDep, ContextCompat.getColor(this,R.color.divider));
+            setDividerColor(npDep, ContextCompat.getColor(this, R.color.divider));
         }
     }
 
+    //*****************normal stuff****************
+
+    /**
+     * set divider color of departure time picker to make it invisible when
+     * to == from
+     * @param picker the numberpicker
+     * @param color colour to make it
+     */
     private void setDividerColor(NumberPicker picker, int color) {
 
         java.lang.reflect.Field[] pickerFields = NumberPicker.class.getDeclaredFields();
@@ -336,7 +336,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public String[] currentDepartures() {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm", java.util.Locale.getDefault());
-        Calendar[] calendars = currentDeparturesCal();
+        Calendar[] calendars = getNSDepartures();
         String[] deps = new String[calendars.length];
         for (int i = 0; i < calendars.length; i++) {
             if (calendars[i] != null) { //if from == to, add empty strings
@@ -349,7 +349,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Departure times magik values are in here
+     * Used to generate strings to show in the numberpicker, and reused when sending to whatsapp,
+     * to find out which time was picked, using the instance id set by the numberpicker
+     *
      * @return calendar array of current departures, null objects if to==from
      */
     public Calendar[] currentDeparturesCal() {
@@ -396,9 +398,9 @@ public class MainActivity extends AppCompatActivity {
         return dep;
     }
 
-
     /**
      * Sends the text, called on buttonclick
+     *
      * @param view button Send
      */
 
@@ -437,7 +439,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * @param c calendar object, probably the one chosen by the time nrpicker
+     * @param c      calendar object, probably the one chosen by the time nrpicker
      * @param travel optional travel time
      * @return added travel time to calendar object and converted to string
      */
@@ -449,6 +451,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * departure time should be the first departure time in a given hour
+     *
      * @param depart Train departure time
      * @param offset Total travel time
      * @return departure time with offset
@@ -480,6 +483,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * send message to whatsapp
+     *
      * @param text the message
      */
     public void sendText(String text) {
@@ -489,5 +493,77 @@ public class MainActivity extends AppCompatActivity {
         sendIntent.setType("text/plain");
         sendIntent.setPackage("com.whatsapp");
         startActivity(sendIntent);
+    }
+
+
+    class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
+
+
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        protected String doInBackground(Void... urls) {
+            InputStream in;
+            int resCode;
+
+            try {
+                //http://t.m.schouten@student.tue.nl:sO-65AZxuErJmmC28eIRB85aos7oGVJ0C6tOZI9YeHDPLXeEv1nfBg@webservices.ns.nl/ns-api-treinplanner?fromStation=Roosendaal&toStation=Eindhoven
+
+                //build right url using to and from
+//                int from = getThisFrom();
+//                int to = getThisTo();
+
+                if (to == from) {
+                    Log.e("asynctask", "no url possible");
+                    return "no url possible";
+                } else {
+                    String fromString = convertToFromToString(from);
+                    String toString = convertToFromToString(to);
+                    URL url = new URL("http://webservices.ns.nl/ns-api-treinplanner?fromStation="
+                            + fromString + "&toStation=" + toString);
+//                    URL url = new URL("http://webservices.ns.nl/ns-api-treinplanner?fromStation=Roosendaal&toStation=Eindhoven");
+
+//                String userCredentials = "t.m.schouten@student.tue.nl:sO-65AZxuErJmmC28eIRB85aos7oGVJ0C6tOZI9YeHDPLXeEv1nfBg";
+//                String encoding = new String(android.util.Base64.encode(userCredentials.getBytes(), Base64.DEFAULT));
+//                encoding = encoding.replaceAll("\\s+",""); //because the base64 encoding doesn't work.
+
+                    //encoded userCredentials with online encoder
+                    String encoding = "dC5tLnNjaG91dGVuQHN0dWRlbnQudHVlLm5sOnNPLTY1QVp4dUVySm1tQzI4ZUlSQjg1YW9zN29HVkowQzZ0T1pJOVllSERQTFhlRXYxbmZCZw==";
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                    urlConnection.setRequestProperty("Authorization", "Basic " + encoding);
+
+                    try {
+                        resCode = urlConnection.getResponseCode();
+                        if (resCode == HttpURLConnection.HTTP_OK) {
+                            in = urlConnection.getInputStream();
+                        } else {
+                            Log.e("rescode", "rescode not ok");
+                            in = urlConnection.getErrorStream();
+                        }
+                        BufferedReader bufferedReader = new BufferedReader(
+                                new InputStreamReader(in));
+                        StringBuilder stringBuilder = new StringBuilder();
+                        String line;
+                        while ((line = bufferedReader.readLine()) != null) {
+                            stringBuilder.append(line).append("\n");
+                        }
+                        bufferedReader.close();
+                        return stringBuilder.toString();
+                    } finally {
+                        urlConnection.disconnect();
+                    }
+                }
+            } catch (Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String response) {
+            progressBar.setVisibility(View.GONE);
+            setResponse(response);
+            updateDepartures(); //update nrpicker
+        }
     }
 }
