647e325e1f4393127477f16413542fad411288f1
Thomas Schouten <hollandpirates@gmail.com>
2016-04-08 19:24:33 +0200

    fixed #37 bootreceiver

---
+++ AppHome/app/src/main/java/com/abbyberkers/apphome/BootReceiver.java
@@ -0,0 +1,73 @@
+package com.abbyberkers.apphome;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import java.util.Calendar;
+
+public class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent i) {
+        int[] direction = WidgetSettings.loadDirection(context);
+
+        Calendar cal = nextDeparture(direction);
+        cal.add(Calendar.MINUTE, 1);
+
+        Intent intent = new Intent(context, Receiver.class);
+
+        PendingIntent pIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), 1800000, pIntent);
+
+        Log.e("end ", "of onReceive");
+
+    }
+
+    Calendar nextDeparture(int[] direction) {
+        int EHV = 0;
+        int Heeze = 1;
+        int RDaal = 2;
+
+        int from = direction[0];
+        int to = direction[1];
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+
+        int minutes = cal.get(Calendar.MINUTE);
+        int depart = 0;
+
+        if (from == EHV) {
+            if (to == Heeze) {
+                depart = 4;
+            } else if (to == RDaal) {
+                depart = 1;
+            }
+        } else if (from == Heeze) {
+            if (to == EHV || to == RDaal) {
+                depart = 15;
+            }
+        } else if (from == RDaal) {
+            if (to == EHV || to == Heeze) {
+                depart = 20;
+            }
+        }
+
+        if (minutes < depart) {
+            cal.set(Calendar.MINUTE, depart);
+        } else if (depart < minutes && minutes < depart + 30) {
+            cal.set(Calendar.MINUTE, depart + 30);
+        } else {
+            cal.set(Calendar.MINUTE, depart);
+            cal.add(Calendar.MINUTE, 60);
+        }
+
+        return cal;
+    }
+}
