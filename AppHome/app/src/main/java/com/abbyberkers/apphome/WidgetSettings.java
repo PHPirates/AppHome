775a7a027e2c781b5d7c80d9487ed25719c4a49d
Thomas Schouten <hollandpirates@gmail.com>
2016-04-10 11:21:15 +0200

    added a little more comments

--- AppHome/app/src/main/java/com/abbyberkers/apphome/WidgetSettings.java
+++ AppHome/app/src/main/java/com/abbyberkers/apphome/WidgetSettings.java
@@ -8,11 +8,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.NumberPicker;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class WidgetSettings extends AppCompatActivity {
@@ -78,7 +78,7 @@ public class WidgetSettings extends AppCompatActivity {
      *
      * @return next departure time
      */
-    Calendar nextDeparture(){
+    Calendar nextDeparture() {
         Calendar cal = Calendar.getInstance();
         cal.set(Calendar.SECOND, 0);
         cal.set(Calendar.MILLISECOND, 0);
@@ -116,22 +116,27 @@ public class WidgetSettings extends AppCompatActivity {
 
     /**
      * on buttonclick to set widget,
+     *
      * @param view button setWidget
      */
-    public void setWidget(View view){
+    public void setWidget(View view) {
 
+        //save direction chose by numberpickers
         saveDirection(context, from, to);
+
+        //duplicate from code in WidgetProvider.onReceive
 
         Calendar cal = nextDeparture();
         cal.add(Calendar.MINUTE, 1);
 
+        //set alarm to update every half an hour
         Intent intent = new Intent(context, Receiver.class);
-
         PendingIntent pIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
 
-        AlarmManager alarmManager = (AlarmManager)context.getSystemService(ALARM_SERVICE);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(ALARM_SERVICE);
         alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), 1800000, pIntent);
 
+        //standard code block to update widget
         AppWidgetManager appWidgetManager = AppWidgetManager
                 .getInstance(context);
         ComponentName thisAppWidget = new ComponentName(context
@@ -144,35 +149,40 @@ public class WidgetSettings extends AppCompatActivity {
         updateIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS,
                 appWidgetIds);
         context.sendBroadcast(updateIntent);
-        // Done with Configure, finish all activities.
-        finishAffinity();
-
+        // Done with Configure, finish this activity.
+        ActivityCompat.finishAffinity(this);
     }
+
+//    /**
+//     * @param c calendar object
+//     * @return c in a string
+//     */
+//    public String cToString(Calendar c) {
+//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm", java.util.Locale.getDefault());
+//        return simpleDateFormat.format(c.getTime());
+//    }
 
     /**
-     * @param c calendar object
-     * @return c in a string
+     * Save direction into shared prefs
+     *
+     * @param context context for prefs
+     * @param from    from city
+     * @param to      to city
      */
-    public String cToString(Calendar c) {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm", java.util.Locale.getDefault());
-        return simpleDateFormat.format(c.getTime());
-    }
-
-    static void saveDirection(Context context, String direction){
-        SharedPreferences.Editor prefs = context.getSharedPreferences(
-                PREFS_NAME,0).edit();
-        prefs.putString(DIRECTION_KEY, direction);
-        prefs.commit();
-    }
-
     static void saveDirection(Context context, int from, int to) {
         SharedPreferences.Editor prefs = context.getSharedPreferences(
-                PREFS_NAME, MODE_MULTI_PROCESS).edit();
+                PREFS_NAME, 0).edit(); //0 instead of MODE_MULTI_PROCESS
         prefs.putInt("from", from);
         prefs.putInt("to", to);
         prefs.apply();
     }
 
+    /**
+     * Load direction from shared prefs
+     *
+     * @param context context for prefs
+     * @return int[] with first is from, second is to
+     */
     static int[] loadDirection(Context context) {
         SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, 0);
         int[] fromto = new int[2];
@@ -180,28 +190,4 @@ public class WidgetSettings extends AppCompatActivity {
         fromto[1] = prefs.getInt("to", -1);
         return fromto;
     }
-
-    String direction(int from, int to){
-        if(from == EHV){
-            if(to == Heeze){
-                return "Eindhoven - Heeze";
-            } else if(to == RDaal){
-                return "Eindhoven - Roosendaal";
-            }
-        } else if(from == Heeze){
-            if(to == EHV){
-                return "Heeze - Eindhoven";
-            } else if(to == RDaal){
-                return "Heeze - Roosendaal";
-            }
-        } else {
-            if(to == EHV){
-                return "Roosendaal - Eindhoven";
-            } else if(to == Heeze){
-                return "Roosendaal - Heeze";
-            }
-        }
-        return "from - to";
-    }
 }
-
