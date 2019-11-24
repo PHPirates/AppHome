797e959596c86692a02095e11b0d9e1415cb1bc8
Abby Berkers <abbyberkers@gmail.com>
2019-11-24 18:38:55 +0100

    Fix delay string

--- app/src/main/java/com/abbyberkers/apphome/ns/json/Trip.kt
+++ app/src/main/java/com/abbyberkers/apphome/ns/json/Trip.kt
@@ -6,11 +6,14 @@ class Trip {
     var actualDurationInMinutes: Int = 0
 
     // A trip consists of one or more legs.
-    var legs: List<Leg>? = null
+    private var legs: List<Leg>? = null
 
     // Get the planned departure and arrival times, as actual times are probably not known yet.
     fun departureTime(): String? = legs?.first()?.origin?.plannedDateTime
     fun arrivalTime(): String? = legs?.last()?.destination?.plannedDateTime
     // Compute the delay as the difference between the actual and planned duration of the trip.
-    fun delay(): String = "+" + (actualDurationInMinutes - plannedDurationInMinutes).toString()
+    fun delay(): String {
+        val delay = actualDurationInMinutes - plannedDurationInMinutes
+        return if (delay > 0) "+" + (actualDurationInMinutes - plannedDurationInMinutes).toString() else ""
+    }
 }
